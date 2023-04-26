0aedd178572f38286752c190763bd927066dc757
Roberto Fonseca Alves <roberto.alves@rhfactor.com.br>
2023-04-26 12:18:13 -0300

    Update ContextMenu.vue

--- src/components/Menu/ContextMenu.vue
+++ src/components/Menu/ContextMenu.vue
@@ -1,10 +1,6 @@
 <template>
   <q-list bordered class="rounded-borders">
-
-
-
     <div v-for="({label, icon, inset, children}, index) in menus" :key="index">
-
       <q-item v-if="children == null || children.length == 0" :to="to" v-ripple clickable>
         <q-item-section avatar>
           <q-icon :name="icon"/>
@@ -13,7 +9,6 @@
           <q-item-label>{{ label }}</q-item-label>
         </q-item-section>
       </q-item>
-
       <q-expansion-item
           v-else
           expand-separator
@@ -21,30 +16,22 @@
           :icon="icon"
           :label="label"
       >
-
         <q-item v-for="({label: cLabel, to}, cIndex) in children" :key="cIndex" :to="to" v-ripple clickable>
           <q-item-section>
             <q-item-label>{{ cLabel }}</q-item-label>
           </q-item-section>
         </q-item>
-
       </q-expansion-item>
     </div>
-
-
-
-
-
-
   </q-list>
 </template>
 
 <script setup>
 
 const menus = [
-  {icon: 'today', label: 'Agendar', inset: 0},
-  {icon: 'calendar_month', label: 'Meus agendamentos', inset: 0},
-  {icon: 'person', label: 'Meu perfil', inset: 0},
+  {to: '/schedules/add', icon: 'today', label: 'Agendar', inset: 0},
+  {to: '/schedules', icon: 'calendar_month', label: 'Meus agendamentos', inset: 0},
+  {to: '/profile', icon: 'person', label: 'Meu perfil', inset: 0},
   {
     icon: 'settings', label: 'Configurações', inset: 0, children: [
       {to: '/settings/users', label: 'Usuários'},
@@ -52,7 +39,6 @@ const menus = [
       {to: '/settings/time', label: 'Tempo'},
       {to: '/settings/rooms', label: 'Quadras'},
       {to: '/settings/clients', label: 'Clientes'}
-
     ]
   },
 ]
